11bb75671c6637e544bf4fc1876f8559b2cb6dd8
Anthony Tabano <anthony.tabano@wipro.com>
2023-08-12 15:09:40 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,76 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Html5QrcodeScanner} from 'html5-qrcode';
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Html5QrcodeError} from "html5-qrcode/src/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'HealthScan-UI';
+export class AppComponent implements AfterViewInit {
+    @ViewChild('scanner') scannerElement!: ElementRef;
+    qrResultString: string | null = null;
+    deviceLocation: any;
+    qrCodeMetadata: any | null = null;
+
+    private html5QrcodeScanner!: Html5QrcodeScanner;
+
+    constructor(private http: HttpClient, private snackBar: MatSnackBar) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                this.deviceLocation = {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                };
+            });
+        }
+    }
+
+
+    ngAfterViewInit() {
+        const scannerId = this.scannerElement.nativeElement.id;
+
+        const config = {
+            fps: 10,
+            qrbox: {width: 400, height: 250},
+            rememberLastUsedCamera: true,
+            showTorchButtonIfSupported: true
+        };
+
+        this.html5QrcodeScanner = new Html5QrcodeScanner(scannerId, config, false);
+    }
+
+    startScanning() {
+        const onScanSuccess = (decodedText: string, decodedResult: any) => {
+            this.qrResultString = decodedText;
+            // Capture the metadata
+            this.qrCodeMetadata = {
+                userAgent: navigator.userAgent,
+                platform: navigator.platform,
+                location: this.deviceLocation
+            };
+
+            // Get the IP address from a public API
+            this.http.get<{ ip: string }>('https://api.ipify.org?format=json').subscribe(response => {
+                this.qrCodeMetadata.ipAddress = response.ip;
+                console.log(`Code matched = ${decodedText}`, this.qrCodeMetadata);
+                this.html5QrcodeScanner.clear();
+            });
+        };
+
+        const onScanError = (errorMessage: string, error: Html5QrcodeError): void => {
+            if (error.type !== 0) {
+                this.openSnackBar(`Scan Error: ${errorMessage}`, '⚠️');
+            }
+        };
+        this.html5QrcodeScanner.render(onScanSuccess, onScanError);
+    }
+
+    openSnackBar(message: string, action: string) {
+        this.snackBar.open(message, action, {
+            duration: 2000,
+        });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {MatCardModule} from "@angular/material/card";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatSnackBarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
